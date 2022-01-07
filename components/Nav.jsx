7056d1595ec0565d503e38b3fb1f147baa2026f7
Oscar Gomez <oscarthedev@gmail.com>
2022-01-07 00:57:35 -0500

    [FINISHED] nav full responsive

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,41 @@
+import Link from 'next/link'
 export default function Nav() {
 	return (
 		<nav>
+			<div className='nav-platforms-container nav-container'>
+				<Link href='/pc'>
+					<a>
+						Pc
+					</a>
+				</Link>
+				<Link href='/play-station'>
+					<a>
+						PlayStation
+					</a>
+				</Link>
+				<Link href='/switch'>
+					<a>
+						Switch
+					</a>
+				</Link>
+			</div>
+			<div className='nav-link-container nav-container'>
+				<Link href="/">
+					<a className='link-home'>
+						Home
+					</a>
+				</Link>
+				<Link href="/about">
+					<a className='link-about'>
+						About
+					</a>
+				</Link>
+				<Link href="/contact-us">
+					<a className='link-contact'>
+						Contact
+					</a>
+				</Link>
+			</div>
 		</nav>
 	)
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,9 +3,9 @@ import '../styles/global.css'
 import '../styles/header/header.css'
 import '../styles/header/searcher.css'
 import '../styles/header/menu/menu.css'
-import '../styles/nav.css'
+import '../styles/nav/nav.css'
 import '../styles/layout.css'
-import '../styles/footer.css'
+import '../styles/footer/footer.css'
 
 export default function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />
