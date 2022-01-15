af382473c46688b505d7a4d4e6fbd9bbdac1a543
Oscar Gomez <oscarthedev@gmail.com>
2022-01-14 21:58:23 -0500

    [FINISHED] modal data recolection completed

--- components/Nav/MyAccount.jsx
+++ components/Nav/MyAccount.jsx
@@ -5,39 +5,84 @@ import Swal1 from 'sweetalert'
 
 export default function MyAccount() {
 	const Swal2 = WithReactContent(NonReactSwal)
-	const registerSwal = async() => {
-		const {value: formValues } = await Swal2.fire({
+	const registerSwal = () => {
+		Swal2.fire({
 			title:"Register",
 			html: `
-				<input id="register-swal-input-1" required="true"type="text" placeholder="Name" class="swal2-input">
-				<input id="register-swal-input-2" required="true" type="text" placeholder="Username" class="swal2-input"/>
-				<input id="register-swal-input-3" required="true" type="email" placeholder="Email" class="swal2-input"/>
-				<input id="register-swal-input-4" required="true" type="password" placeholder="Password" class="swal2-input"/>
+			<form>
+				<input id="register-swal-input-4" type="text" placeholder="Name" class="swal2-input">
+				<input id="register-swal-input-3" type="text" placeholder="Username" class="swal2-input"/>
+				<input id="register-swal-input-2" type="email" placeholder="Email" class="swal2-input"/>
+				<input id="register-swal-input-1" type="password" placeholder="Password" class="swal2-input"/>
+			</form>
 			`,
+			preConfirm: function() {
+				return new Promise((res,rej) =>{
+					const validateField = (idNum, placeholder, email) => {
+						let inputValue = document.getElementById(`register-swal-input-${idNum}`).value
+						if(inputValue){
+							if(email) {
+								if(!inputValue.includes('@')) Swal2.showValidationMessage('Non valid email direction')
+							}
+							return inputValue
+						}
+						else Swal2.showValidationMessage(`${placeholder} data missing`)
+					}
+					res({
+						password: validateField(1,"Password",false),
+						email: validateField(2,"Email",true),
+						username: validateField(3,"Username",false),
+						name: validateField(4,"Name",false),
+					})
+				})
+			},
 			color:"#432",
 			background: "rgb(230,178,77)",
 			confirmButtonColor: "rgb(230,178,77)",
-			preConfirm: () => {
-				return [
-					document.getElementById("register-swal-input-1").value,
-					document.getElementById("register-swal-input-2").value,
-					document.getElementById("register-swal-input-3").value,
-					document.getElementById("register-swal-input-4").value,
-				]
+		}).then(data=>{
+			if(data.value){
+				Swal2.fire({
+					title:'Congratulations your account has been successfully created',
+					text: "You can refresh the page and you should be able to login from this account",
+					background: "rgb(230,178,77)",
+					confirmButtonColor: "rgb(230,178,77)",
+					color: "#432",
+					icon: "success",
+					iconColor: "#432",
+				})
+
 			}
 		})
 	}
-	const loginSwal = async() => {
-		const {value: formValues} = await Swal2.fire({
+	const loginSwal = () => {
+		Swal2.fire({
 			title:"Login",
 			html: `
-				<input type="email" placeholder="Email" class="swal2-input" required="true"/>
-				<input type="password" placeholder="Password" class="swal2-input" required="true"/>
+				<input id="login-swal-input-1" type="email" placeholder="Email" class="swal2-input" required="true"/>
+				<input id="login-swal-input-2" type="password" placeholder="Password" class="swal2-input" required="true"/>
 			`,
 			color:"#432",
-			confirmButtonColor: "rgb(230,178,77)",
-			focusCancel: "true",
 			background: "rgb(230,178,77)",
+			confirmButtonColor: "rgb(230,178,77)",
+			preConfirm: function() {
+				return new Promise((res,rej) =>{
+
+					const validateLoginField = (idNum, placeholder, email) => {
+						let inputValue = document.getElementById(`login-swal-input-${idNum}`).value
+						if(inputValue){
+							if(email) {
+								if(!inputValue.includes('@')) Swal2.showValidationMessage('Non valid email direction')
+							}
+							return inputValue
+						}
+						else Swal2.showValidationMessage(`${placeholder} data missing`)
+					}
+					res({
+						password: validateLoginField(2,"Password", false),
+						email: validateLoginField(1, "Email", true)
+					})
+				})
+			}
 		})
 
 	}
@@ -64,7 +109,7 @@ export default function MyAccount() {
 						loginSwal()	
 					}
 					else {
-						registerSwal()						
+						registerSwal()
 					}
 				})
 			}}
