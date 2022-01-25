2e3ba94c256ab027e8d3bbb5f8844033f59fc502
Oscar Gomez <oscarthedev@gmail.com>
2022-01-24 22:19:37 -0500

    [REFACTOR] /v1/games endpoint now being used

--- pages/wii.jsx
+++ pages/wii.jsx
@@ -1,14 +1,22 @@
 import Layout from "../components/Layout"
 
-export default function Wii() {
-	let arrForImgs = []
-	for(let i = 1; i<11; i++) arrForImgs.push(i)
-	console.log(arrForImgs)
+export default function Wii({data}) {
 	return (
 		<div className='page'>
 			<Layout>
-				{arrForImgs.map(num=><img key={`wii${num}`} src={`http://localhost:4000/v1/wiiGamesImages/wii${num}.jpg`}/>)}	
+				{
+					data.games.map(game=>{
+						if(game.console=="Wii"){
+							return <img src={game.logo} key={game._id} alt={game.name} />
+						}
+					})
+				}	
 			</Layout>
 		</div>
 	)
+}
+export async function getStaticProps() {
+	const response = await fetch("http://localhost:4000/v1/games")
+	const data = await response.json()
+	return {props: {data}}
 }
--- pages/xbox360.jsx
+++ pages/xbox360.jsx
@@ -1,14 +1,22 @@
 import Layout from "../components/Layout"
 
-export default function Xbox360() {
-	let arrForImgs = []
-	for(let i = 1; i<11; i++) arrForImgs.push(i)
-	console.log(arrForImgs)
+export default function Xbox360({data}) {
 	return (
 		<div className='page'>
 			<Layout>
-				{arrForImgs.map(num=><img key={`xbox${num}`} src={`http://localhost:4000/v1/xboxGamesImages/${num}.jpg`}/>)}	
+				{
+					data.games.map(game=>{
+						if(game.console=="Xbox360") {
+							return <img src={game.logo} key={game._id} alt={game.name} />
+						}
+					})
+				}	
 			</Layout>
 		</div>
 	)
+}
+export async function getStaticProps() {
+	const response = await fetch("http://localhost:4000/v1/games")
+	const data = await response.json()
+	return {props: {data}}
 }
