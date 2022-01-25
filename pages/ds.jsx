b07f37bd1b058e6bba13158a89fade6b2f6bab73
Oscar Gomez <oscarthedev@gmail.com>
2022-01-25 01:17:48 -0500

    [FINISHED] swal onClick img advanced

--- pages/ds.jsx
+++ pages/ds.jsx
@@ -1,16 +1,44 @@
 import Layout from "../components/Layout"
+import NonReactSwal from 'sweetalert2'
+import WithReactContent from 'sweetalert2-react-content'
 
 export default function DS({data}) {
+	const Swal2 = WithReactContent(NonReactSwal)
+	const GameSwal = (title, console, desc, img) => {
+		Swal2.fire({
+			title: title + "<br/>" + console,
+			text: desc,
+			color:"#432",
+			background: "rgb(230,178,77)",
+			confirmButtonText: "Add to Cart",
+			confirmButtonColor: "rgb(230,178,77)",
+		})
+	}
 	return (
 		<div className='page'>
 			<Layout ds={true}>
-				{
+			{
 					data.games.map(game=>{
 						if(game.console=="Ds"){
-							return <img src={game.logo} key={game._id} alt={game.name} />
-						}
+							return (
+								<img 
+									src={game.logo}
+									key={game._id}
+									alt={game.name}
+									onClick={
+										()=>{
+											GameSwal(
+												game.name,
+												game.console,
+												game.description,
+												""
+											)
+										}
+									}
+								/>
+							)						}
 					})
-				}	
+				}
 			</Layout>
 		</div>
 	)
@@ -18,5 +46,6 @@ export default function DS({data}) {
 export async function getStaticProps() {
 	const response = await fetch("http://localhost:4000/v1/games")
 	const data = await response.json()
+	console.log(data)
 	return {props: {data}}
 }
--- pages/wii.jsx
+++ pages/wii.jsx
@@ -1,16 +1,42 @@
 import Layout from "../components/Layout"
+import NonReactSwal from 'sweetalert2'
+import WithReactContent from 'sweetalert2-react-content'
 
 export default function Wii({data}) {
+	const Swal2 = WithReactContent(NonReactSwal)
+	const GameSwal = (title, console, desc, img) => {
+		Swal2.fire({
+			title: title + "<br/>" + console,
+			text: desc,
+			color:"#432",
+			background: "rgb(230,178,77)",
+			confirmButtonText: "Add to Cart",
+			confirmButtonColor: "rgb(230,178,77)",
+		})
+	}
 	return (
-		<div className='page'>
+		<div className='page' >
 			<Layout>
-				{
+			{
 					data.games.map(game=>{
 						if(game.console=="Wii"){
-							return <img src={game.logo} key={game._id} alt={game.name} />
-						}
+							return (
+								<img 
+									src={game.logo}
+									key={game._id}
+									alt={game.name}
+									onClick={()=>{
+										GameSwal(
+											game.name,
+											game.console,
+											game.description,
+											""
+										)
+									}}	
+								/>
+							)						}
 					})
-				}	
+				}
 			</Layout>
 		</div>
 	)
@@ -18,5 +44,6 @@ export default function Wii({data}) {
 export async function getStaticProps() {
 	const response = await fetch("http://localhost:4000/v1/games")
 	const data = await response.json()
+	console.log(data)
 	return {props: {data}}
 }
