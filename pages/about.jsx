89d4ed1677f31fe5c4efe0fd1ce6808a269b8074
Oscar Gomez <oscarthedev@gmail.com>
2022-01-27 00:43:33 -0500

    [ADD] new routes

---
+++ pages/about.jsx
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout"
+
+export default function About() {
+	return (
+		<div className='page'>
+			<Layout singlePage={true} needsSearcher={false}>
+				<main>
+					<h2>About</h2>
+					<p></p>
+				</main>
+			</Layout>
+		</div>
+	)
+}
---
+++ pages/cart.jsx
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout"
+
+export default function Cart() {
+	return (
+		<div className='page'>
+			<Layout singlePage={true} needsSearcher={false}>
+				<main>
+					<h2>Cart</h2>
+					<p></p>
+				</main>
+			</Layout>
+		</div>
+	)
+}
