b1d746ec3a7c0640ae14de262dc5382b82f0ce54
Oscar Gomez <oscarthedev@gmail.com>
2022-02-18 17:32:02 -0500

    [PATCH] user page now using redux instead of reactContext

--- pages/user.jsx
+++ pages/user.jsx
@@ -1,18 +1,22 @@
 import Layout from "../components/Layout"
-import {useContext} from 'react'
-import AuthContext from "../context/AuthContext"
+import {useSelector} from 'react-redux'
+import { useEffect } from "react"
 
 export default function User() {
-	const {auth} = useContext(AuthContext)
+	const log = useSelector(s=>s.log)
+	useEffect(() => {
+		console.log(log)
+	}, [])
+	
 	return (
 		<div className='page'>
 			<Layout singlePage={true} needsSearcher={false}>
 				<main className="main-user">
 					<h2>Your Information:</h2>
-					<div> {auth ? "Username: " + auth.user.username : "You've not logged in yet"} </div>
-					<div> {auth ? "Name: " + auth.user.name : ""} </div>
-					<div> {auth ? "Email: " + auth.user.email : ""} </div>
-					<div> {auth ? "Country: " + auth.user.country : ""} </div>
+					<div> {log.user ? "Username: " + log.user.username : "You've not logged in yet"} </div>
+					<div> {log.user ? "Name: " + log.user.name : ""} </div>
+					<div> {log.user ? "Email: " + log.user.email : ""} </div>
+					<div> {log.user ? "Country: " + log.user.country : ""} </div>
 				</main>
 			</Layout>
 		</div>
