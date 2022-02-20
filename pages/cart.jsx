c50e4b6f4b503fd41772ddc5fefe4c7e6e292ff8
Oscar Gomez <oscarthedev@gmail.com>
2022-02-20 16:56:34 -0500

    [ADD] cart page now can stack games from userData

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,6 +1,21 @@
 import Layout from "../components/Layout"
+import { useState, useEffect } from "react"
+//redux
+import { logger } from '../redux/actions'
+import { useSelector, useDispatch } from "react-redux"
+import CartObject from "../components/cart/cartObject"
 
 export default function Cart() {
+	const dispatch = useDispatch()
+	const log = useSelector(s=>s.log)
+	const [cartList, setCartList] = useState(undefined)
+	
+	useEffect(() => {
+		console.log(log ? log.user.gamesCart : [])
+		setCartList(log ? log.user.gamesCart : [])
+	}, [log]);
+
+	if(cartList === undefined ) return null
 	return (
 		<div className='page'>
 			<Layout singlePage={true} needsSearcher={false}>
@@ -8,7 +23,18 @@ export default function Cart() {
 					<div className="main-cart-title">
 						<h2>Cart</h2>
 					</div>
-					<div className="main-cart-content"></div>
+					<div className="main-cart-content">
+						<div className="main-cart-content-list">
+							{
+								cartList.map((game, index)=>{
+									return (
+										<CartObject index={index} game={game}/>
+									)
+								})
+							}
+						</div>
+						<div className="main-cart-content-pay"></div>
+					</div>
 				</main>
 			</Layout>
 		</div>
