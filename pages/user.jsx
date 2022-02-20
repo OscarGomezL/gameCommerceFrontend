d36bb5b87db9b30d0fa83ebdc5c2793fda6c23df
Oscar Gomez <oscarthedev@gmail.com>
2022-02-19 19:29:07 -0500

    [ADD] css stuff to user page

--- pages/user.jsx
+++ pages/user.jsx
@@ -12,11 +12,15 @@ export default function User() {
 		<div className='page'>
 			<Layout singlePage={true} needsSearcher={false}>
 				<main className="main-user">
-					<h2>Your Information:</h2>
-					<div> {log.user ? "Username: " + log.user.username : "You've not logged in yet"} </div>
-					<div> {log.user ? "Name: " + log.user.name : ""} </div>
-					<div> {log.user ? "Email: " + log.user.email : ""} </div>
-					<div> {log.user ? "Country: " + log.user.country : ""} </div>
+					<div className="main-user-title">
+						<h2>Your Information:</h2>
+					</div>
+					<div className="main-user-content">
+						<div> {log.user ? "Username: " + log.user.username : "You've not logged in yet"} </div>
+						<div> {log.user ? "Name: " + log.user.name : ""} </div>
+						<div> {log.user ? "Email: " + log.user.email : ""} </div>
+						<div> {log.user ? "Country: " + log.user.country : ""} </div>
+					</div>
 				</main>
 			</Layout>
 		</div>
