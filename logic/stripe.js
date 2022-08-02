2470e2c97ce5da7ab6ca0b04548ffc81e2bf7e04
Oscar Gomez <oscarthedev@gmail.com>
2022-08-02 16:16:58 -0600

    [ADD] stripe "buy" function is now working with the parameters send by "cart" page

--- logic/stripe.js
+++ logic/stripe.js
@@ -1,16 +1,14 @@
 export const buy = (data) => {
 	console.log(data)
+	let items = data.filter(el=>el!=="").map(game=> {
+		return {id: game.listNum, quantity: game.quantity}
+	})
 	fetch('http://localhost:4000/v1/stripe/create-checkout-session', {
 		method: 'POST',
 		headers: {
 			'Content-Type':'application/json'
 		},
-		body: JSON.stringify({
-			items: [
-				{ id: 1, quantity: 3},
-				{ id: 2, quantity: 1},
-			]
-		}),
+		body: JSON.stringify({items}),
 	}).then(res=> {
 		if(res.ok) return res.json()
 		return res.json().then(json=>Promise.reject(json))
