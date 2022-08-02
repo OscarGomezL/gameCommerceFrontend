a80b7bddea41d6b49ce1e510b65a486fcb9a2741
Oscar Gomez <oscarthedev@gmail.com>
2022-08-02 17:20:54 -0600

    [PATCH] about page description improved

--- pages/about.jsx
+++ pages/about.jsx
@@ -6,7 +6,16 @@ export default function AboutDeveloper() {
 			<Layout singlePage={false} needsSearcher={false}>
 				<main className="main-about">
 					<h2>About</h2>
-					<p>GameCommerce is one of the personal projects I'm doing on my free time. It's an ecommerce realistic simulation which can actually work as a functional ecommerce in production. In GameCommerce you're provided with a fake card which works with Stripe as a real credit or debit card with infinite fake money so the user can “order” whichever game he wants and assign such order to a direction just like he would on an actual ecommerce.</p>
+					<p>GameCommerce is one of the personal projects I'm doing on my free time. It's an ecommerce realistic simulation which can actually work as a functional ecommerce in production. In GameCommerce you're provided with a fake card which works with Stripe as a real card just so the user can complete payments like he would on an actual ecommerce.</p>
+					<h3>To complete payments you have to:</h3>
+					<div className="main-about-requirements">
+						<ul>
+							<li>Use this card: 4242424242424242</li>
+							<li>Use any future date</li>
+							<li>Use any CVC</li>
+							<li>Use any value for the rest of the form spaces</li>
+						</ul>
+					</div>
 				</main>
 			</Layout>
 		</div>
